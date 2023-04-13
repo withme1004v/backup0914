f551241f35a7f684a21940193010b358d840c25d
withme1004v <110023308+withme1004@users.noreply.github.com>
2023-04-13 16:55:23 +0900

    first commit

---
+++ javascript/jQuery/owlcarousel/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+  });
+});
---
+++ javascript/jQuery/owlcarousel/js/main2.js
@@ -0,0 +1,13 @@
+$(function () {
+  $(".loop").owlCarousel({
+    center: true,
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 2,
+    autoWidth: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+  });
+});
